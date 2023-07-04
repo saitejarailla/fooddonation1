404a41b50895ca4e2d8055fa14f237244a8b23a0
saiteja <saitejarailla@gmail.com>
2023-07-04 17:06:56 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/header'
+import Footer from './components/footer';
+import DonateDel from './components/donateDel';
+import './App.css'
+ 
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
